ba3bbb258a000548c8d84051f5f9596036f4bb95
Fatima <sayyedafatima.official@gmail.com>
2023-03-14 09:32:09 +0500

    Context created successfully

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,3 +1,6 @@
+import React,{useState, useEffect} from 'react';
+
+
 import image0 from "../Images/596296.jpeg";
 import image1 from "../Images/681016.jpg";
 import image2 from "../Images/976013.jpg";
@@ -5,34 +8,67 @@ import image3 from "../Images/987919.jpg";
 import image4 from "../Images/mosque.jpg";
 import image5 from "../Images/rain.jpg";
 
-const getAllImages = () =>{
-    const arr = [image0, image1, image2, image3, image4, image4, image5];        
-        const images = arr.map((item, i) => {
-            return (
-                <>
-                    <div className="single-post-container" key={i}>
-                        <span className="account-name">My Account</span>
-                        <div className="image-container">
-                                <a href={item} key={i} >
-                                <img
-                                    key={i}
-                                    src={item}
-                                    alt="new"
-                                    className="image-list"
-                                ></img>
-                            </a>
+export const InscribleContext=React.createContext();
+
+
+export const InscribleProvider=({children})=>{
+
+    // const [formData, setformData] = useState({
+    //     description: ""
+    // });
+
+    const [postsArr, setpostsArr] = useState([]);
+
+    // const [connectedAccounts, setconnectedAccounts] = useState("");
+
+    // const [isConnected, setisConnected] = useState(false)
+    const [isLoading, setisLoading] = useState(false);
+
+    const getAllImages = async() =>{
+        setisLoading(true);
+        const arr = [image0, image1, image2, image3, image4, image4, image5];        
+       const images = arr.map((item, i) => {
+                return (
+                    <>
+                        <div className="single-post-container" key={i}>
+                            <span className="account-name">My Account</span>
+                            <div className="image-container">
+                                    <a href={item} key={i} >
+                                    <img
+                                        key={i}
+                                        src={item}
+                                        alt="new"
+                                        className="image-list"
+                                    ></img>
+                                </a>
+                            </div>
+                            <span className="account-name" >My Account:
+                                <span className="description-text">This is the discription of my image</span>
+                            </span>
                         </div>
-                        <span className="account-name" >My Account:
-                            <span className="description-text">This is the discription of my image</span>
-                        </span>
-                    </div>
-                    <hr />
-                </>
-            );
-        });
+                        <hr />
+                    </>
+                );
+            });
 
-        return images;
-        
+            setTimeout(() => {
+                setisLoading(false);
+            }, 10000);
+
+            setpostsArr(images);
+            
+    }
+
+    useEffect(() => {
+        getAllImages();
+    },[]);
+
+    return(
+        <InscribleContext.Provider value={{
+        isLoading,
+        postsArr,
+       getAllImages}}>
+            {children}
+        </InscribleContext.Provider>
+    );
 }
-
-export default getAllImages;
